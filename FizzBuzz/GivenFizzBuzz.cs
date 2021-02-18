66181ca0263557988f60c99e458b44253eb40f20
Karam Kabbara <contact@karam.io>
2021-02-18 19:08:12 +0000

    🔴 Refactor step has caused failing test since 5 is multiple of both 3 and 5.

--- FizzBuzz/GivenFizzBuzz.cs
+++ FizzBuzz/GivenFizzBuzz.cs
@@ -46,8 +46,7 @@ namespace FizzBuzz
     {
         public static string Convert(int number)
         {
-            if (number == 5) return "Buzz";
-            if (number == 10) return "Buzz";
+            if (number % 5 == 0) return "Buzz";
             if (number % 3 == 0) return "Fizz";
             return number.ToString();
         }
