f6ce3a6855df4fc5bb560bc56f8486fe5652cf70
Karam Kabbara <contact@karam.io>
2021-02-18 19:14:57 +0000

    🔵 Refactor step for multiples of 3 & 5 to return FizzBuzz.

--- FizzBuzz/GivenFizzBuzz.cs
+++ FizzBuzz/GivenFizzBuzz.cs
@@ -54,8 +54,7 @@ namespace FizzBuzz
     {
         public static string Convert(int number)
         {
-            if (number == 15) return "FizzBuzz";
-            if (number == 30) return "FizzBuzz";
+            if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
             if (number % 5 == 0) return "Buzz";
             if (number % 3 == 0) return "Fizz";
             return number.ToString();
