01237e66797b63f9e3d940c51e847c1a9a92e616
Karam Kabbara <contact@karam.io>
2021-02-18 19:06:25 +0000

    🟢 Passing test for 5 to Buzz.

--- FizzBuzz/GivenFizzBuzz.cs
+++ FizzBuzz/GivenFizzBuzz.cs
@@ -39,6 +39,7 @@ namespace FizzBuzz
     {
         public static string Convert(int number)
         {
+            if (number == 5) return "Buzz";
             if (number % 3 == 0) return "Fizz";
             return number.ToString();
         }
