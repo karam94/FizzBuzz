e1f0f9c08a14be094011478d26a991f1c95cf861
Karam Kabbara <contact@karam.io>
2021-02-18 19:13:53 +0000

    🟢 Passing test for 30 to FizzBuzz.

--- FizzBuzz/GivenFizzBuzz.cs
+++ FizzBuzz/GivenFizzBuzz.cs
@@ -55,6 +55,7 @@ namespace FizzBuzz
         public static string Convert(int number)
         {
             if (number == 15) return "FizzBuzz";
+            if (number == 30) return "FizzBuzz";
             if (number % 5 == 0) return "Buzz";
             if (number % 3 == 0) return "Fizz";
             return number.ToString();
